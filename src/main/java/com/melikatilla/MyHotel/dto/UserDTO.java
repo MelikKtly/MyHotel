9347db230fe153116b90cc0099f3b03fb337330a
MelikKtly <melikatilla@hotmail.com>
2025-10-03 10:56:51 +0300

    UserDTO

---
+++ src/main/java/com/melikatilla/MyHotel/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.melikatilla.MyHotel.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.melikatilla.MyHotel.entity.Booking;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private List<Booking>  bookings=new ArrayList<>();
+}
