5e8f8e8b98975611aa92184e1417d21237899c55
MelikKtly <melikatilla@hotmail.com>
2025-10-03 11:03:50 +0300

    DTO update

--- src/main/java/com/melikatilla/MyHotel/dto/BookingDTO.java
+++ src/main/java/com/melikatilla/MyHotel/dto/BookingDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingDTO {
 
-    private int id;
+    private Long id;
     private LocalDateTime checkInDate;
     private LocalDateTime checkOutDate;
     private int numOfAdults;
--- src/main/java/com/melikatilla/MyHotel/dto/RoomDTO.java
+++ src/main/java/com/melikatilla/MyHotel/dto/RoomDTO.java
@@ -1,4 +1,19 @@
 package com.melikatilla.MyHotel.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoomDTO {
+    private Long id;
+    private String roomType;
+    private String roomPrice;
+    private String roomDescription;
+    private String roomPhotoUrl;
+
+    private List<BookingDTO> bookings;
+
 }
